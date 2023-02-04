d1883bcd2e5af88cf41b0f120d1a402ba05e33e5
Никита Лукин <gonka04@gmail.com>
2023-02-05 01:31:16 +0300

    Тестовое задание.

---
+++ MyLib/MyLib/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class Circle : Figure
+    {
+
+        float _radius;
+
+        public float Radius
+        {
+            get => _radius;
+        }
+
+        public Circle(float radius)
+        {
+            if(radius <= 0)
+            {
+                throw new Exception("Circle's radius must be non_negative");
+            }
+            _radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * _radius * _radius;
+        }
+    }
+}
---
+++ MyLib/MyLib/Triangle.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class Triangle : Figure
+    {
+        float _firstSide;
+        float _secondSide;
+        float _thirdSide;
+        bool _isRight;
+
+        public float FirstSide
+        {
+            get => _firstSide;
+        }
+
+        public float SecondSide
+        {
+            get => _secondSide;
+        }
+
+        public float ThirdSide
+        {
+            get => _thirdSide;
+        }
+
+        public bool IsRight
+        {
+            get => _isRight;
+        }
+
+
+        public Triangle(float first, float second, float third)
+        {
+            if(first <= 0 || second <= 0 || third <= 0)
+            {
+                throw new Exception("Triangle's side must be non-negative");
+            }
+            _firstSide = first;
+            _secondSide = second;
+            _thirdSide = third;
+            _isRight = CheckIsRight();
+        }
+
+        private bool CheckIsRight()
+        {
+            float maxSide = new float[3] { _firstSide, _secondSide, _thirdSide }.Max();
+            float maxSideSqr = maxSide * maxSide;
+            return maxSideSqr + maxSideSqr == _firstSide * _firstSide + _secondSide * _secondSide + _thirdSide * _thirdSide;
+        }
+
+        public override double Area()
+        {
+            float p = (_firstSide + _secondSide + _thirdSide) / 2;
+
+            return Math.Sqrt(p * (p - _firstSide) * (p - _secondSide) * (p - _thirdSide));
+        }
+    }
+}
